eb145c65874d7636bd96b92b0927ffed295dafc5
RuslanMirov <ruslan777mirov@gmail.com>
2021-07-13 14:13:14 +0300

    Comment trade

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const dexType = 0 // 1inch
 const fund = new Fund(privateKey, fundAddress, dexType)
 
 // define some tokens
-const bCOT = "0x304fC73e86601a61a6C6db5B0eAfEA587622acdC"
 const BUSD = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56"
 
 // Run your code here
--- methods/trade_from_eth.js
+++ methods/trade_from_eth.js
@@ -8,22 +8,30 @@ const getOneInchData = require("../utils/getOneInchData")
 const ETH_ADDRESS = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
 
 const createTx = async (key, fundAddress, amount, toToken, minReturn, dexType) => {
-  const amountInWei = web3.utils.toWei(amount)
-  const from = privateKeyToAccount(key)
+  // get fund contract instance
   const contract = new web3.eth.Contract(ETH_FUND_ABI, fundAddress)
+
+  // convert ETH input to wei
+  const amountInWei = web3.utils.toWei(amount)
+
+  // get from address
+  const from = privateKeyToAccount(key)
+
+  // get merkle three data
   const {
     proof,
     positions
   } = getMerkleTreeData(toToken)
 
+  // get additional data (require for 1 inch case)
   let additionalData = "0x"
-
   // 1 inch case
   if(dexType === 0){
     const exchangePortal = await contract.methods.exchangePortal().call()
     additionalData = await getOneInchData(ETH_ADDRESS, toToken, amountInWei, exchangePortal)
   }
 
+  // prepare transaction data
   const data = contract.methods.trade(
     ETH_ADDRESS,
     amountInWei,
@@ -35,8 +43,10 @@ const createTx = async (key, fundAddress, amount, toToken, minReturn, dexType) =
     minReturn
   ).encodeABI({from})
 
+  // get nonce number
   const nonce = await web3.eth.getTransactionCount(from)
 
+  // create tx object
   const tx = {
     from,
     to: fundAddress,
@@ -48,13 +58,17 @@ const createTx = async (key, fundAddress, amount, toToken, minReturn, dexType) =
     nonce: nonce
   }
 
+  // return tx object
   return tx
 }
 
 module.exports = async (key, fundAddress, amount, toToken, minReturn, dexType) => {
+  // get tx data
   const tx = await createTx(key, fundAddress, amount, toToken, minReturn, dexType)
+  // sign tx with private key
   const signed  = await web3.eth.accounts.signTransaction(tx, key)
+  // send tx
   const receipt = await web3.eth.sendSignedTransaction(signed.rawTransaction)
-
+  // return result
   return receipt.transactionHash
 }
