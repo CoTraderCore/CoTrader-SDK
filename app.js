676b2b6eb1fce1c4cf62cfd2868dcb02b5f86518
RuslanMirov <ruslan777mirov@gmail.com>
2021-07-13 14:20:01 +0300

    Update

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
+
+// import fund class
 const Fund = require('./fund')
 
-// fund params
+// define fund params
 const fundAddress = '0x3Ca7Ae3E7bd69546429BD86766A7681a47FBF7d0'
 const privateKey = process.env.KEY
 const dexType = 0 // 1inch
@@ -9,14 +11,16 @@ const dexType = 0 // 1inch
 // create fund instance
 const fund = new Fund(privateKey, fundAddress, dexType)
 
-// define some tokens
-const BUSD = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56"
 
-// put your code here
+// put your code inside this function
 async function app() {
-  // test trade from eth to busd
-  const hash = await fund.tradeFromETH(BUSD, "0.0001", 1)
-  // log info 
+  // test trade from bnb to busd token
+  const hash = await fund.tradeFromETH(
+    "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56", // BUSD token address
+    "0.0001", // BNB amount
+    1 // Min return just 1 wei for tests
+  )
+  // log info
   console.log(`Success hash ${hash}`)
 }
 
